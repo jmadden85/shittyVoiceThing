e002ed29d0d6c457108d76e7c6ad8d7edf02a2cc
jmadden85 <jmadden85@gmail.com>
2015-06-30 15:56:15 -0400

    first push of initial getusermedia stuff

---
+++ main.js
@@ -0,0 +1,32 @@
+(function() {
+  function hasGetUserMedia() {
+    return !!(navigator.getUserMedia || navigator.webkitGetUserMedia ||
+              navigator.mozGetUserMedia || navigator.msGetUserMedia);
+  }
+
+  if (hasGetUserMedia()) {
+    // Good to go!
+    //Cross browser functionality
+    navigator.getUserMedia =  navigator.getUserMedia || navigator.webkitGetUserMedia ||
+                              navigator.mozGetUserMedia || navigator.msGetUserMedia;
+
+    var errCallback = function (err) {
+      console.log('Hold onto your butts', err);
+    };
+
+    navigator.getUserMedia({
+      video: true,
+      audio: true
+    }, function (stream) {
+      var video = document.querySelector('video');
+      video.src = window.URL.createObjectURL(stream);
+
+      video.onloadedmetadata = function (e) {
+
+      };
+    }, errCallback);
+
+  } else {
+    alert('This is not supported in your browser');
+  }
+}());
